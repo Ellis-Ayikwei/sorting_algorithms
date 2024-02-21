6174911b552a5b3a0a5130fbc7fb9f9238c2355d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-02-21 02:03:03 +0000

    'fixed the description on num_swap '

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,11 @@
 #include "sort.h"
 
-
+/**
+ * num_swap - Swap two numbers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void num_swap(int *a, int *b);
-void num_swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
 /**
  * lomuto_partition - Order a subset of an array of integers according to
  *                    the lomuto partition scheme (last element as pivot).
